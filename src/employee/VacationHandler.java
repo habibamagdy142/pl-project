7ec0d2b8398464de0c52c6819bba94b993198045
Lama Wael <lamawael35@gmail.com>
2025-12-11 14:05:33 +0200

    Add files via upload

---
+++ src/employee/VacationHandler.java
@@ -0,0 +1,5 @@
+package employee;
+
+public interface VacationHandler {
+    void requestVacation(String empId, String date, String reason);
+}
---
+++ src/employee/WorkingHours.java
@@ -0,0 +1,6 @@
+package employee;
+
+public interface WorkingHours {
+    void entryTime(String empId, String time);
+    void exitTime(String empId, String time);
+}
